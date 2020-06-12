6856aea86c294318d37f5a0dd8427964cd4c82d0
krishan93985 <krisharush93985@gmail.com>
2020-06-12 09:16:31 +0530

    cors bug fixed

--- src/actions.js
+++ src/actions.js
@@ -10,7 +10,12 @@ export const setSearchField = (text) => ({
 
 export const requestRobots = () => (dispatch) => {
 dispatch({type:SEARCH_ROBOTS_PENDING});
-fetch('https://jsonplaceholder.typicode.com/users')
+fetch('https://jsonplaceholder.typicode.com/users',{
+    mode:'cors',
+    headers:{
+        'Access-Control-Allow-Origin':'https://krishan93985.github.io/robofriends/'
+    }
+})
 .then(response => response.json())
 .then(robots => dispatch({type:SEARCH_ROBOTS_SUCCESS,payload:robots}))
 .catch(error => dispatch({type:SEARCH_ROBOTS_FAILED,payload:error}))
