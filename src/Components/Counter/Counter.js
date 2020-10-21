c8e1ec16b042de90fae26036ea4d962924ce4ca1
krishan93985 <krisharush93985@gmail.com>
2020-10-21 15:26:53 +0530

    passing through prettier

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 const Counter = () => {
     const [count,setCount] =
-     useState(0);
+       useState(0);
 
     return (
         <button id="counter" data-count={count} onClick={() => setCount(count+1)} >Count:{count}</button>
